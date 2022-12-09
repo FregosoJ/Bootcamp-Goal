808da89c457d89fee5c4cffcc64e01ab0340ac94
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-09 10:49:40 -0800

    fixed bug in function adjusting raw user input

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
 var addCityButton = document.querySelector("#add-input");
+var searchButton = document.querySelector("#search-button");
 var cityInput = document.querySelector("#city-input");
+var cityList = document.querySelector("#cities-in-chart");
 var urbanAreasApi = "https://api.teleport.org/api/urban_areas/"; 
 
 function sluggifyInput () {
     var rawCityInput = cityInput.value;
+    if (!rawCityInput) {
+        return;
+    }
     console.log(rawCityInput);
     for(var i = 0; i < rawCityInput.length; i++) {
-         var properCityInput = rawCityInput.replace(" ", "-")
+        rawCityInput = rawCityInput.replace(" ", "-")
      }
-    var searchInput = "slug:" + properCityInput.toLowerCase();
+    var searchInput = "slug:" + rawCityInput.toLowerCase();
      console.log(searchInput);
 }
 
