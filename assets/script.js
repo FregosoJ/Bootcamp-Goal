119f642b5298796803be68b10b430bae333ed749
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-09 12:08:08 -0800

    successfully putting city names into local storage, successfully translating city names into ua codes for api, successfully putting ua codes into local storage and pulling them back out in an array format

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
 var addCityButton = document.querySelector("#add-input");
 var searchButton = document.querySelector("#search-button");
+var clearButton = document.querySelector("#clear-button");
 var cityInput = document.querySelector("#city-input");
 var cityList = document.querySelector("#cities-in-chart");
 var salariesApiUrl = "https://api.teleport.org/api/urban_areas/"
 var uaCodes = [];
-var selectedCities= [];
+//var selectedCities=[];
 
 function getUaCodes () {
     uaCodes = localStorage.getItem
@@ -30,12 +31,27 @@ function addCityToSearch () {
     console.log(selectedCities);
     selectedCities.push(rawCityInput);
     localStorage.setItem("selected cities", JSON.stringify(selectedCities));
-    
+    cityList.innerHTML = "";
+    for (var i = 0; i < selectedCities.length; i++){
     var listedCity = document.createElement("li");
-    listedCity.textContent = cityInput.value;
+    listedCity.textContent = selectedCities[i];
     cityList.append(listedCity);
+    }
+    for(var i = 0; i < selectedCities.length; i++) {
+            sluggifiedCity = selectedCities[i].replaceAll(" ", "-")
+            sluggifiedCity = "slug:" + sluggifiedCity.toLowerCase();
+            uaCodes.push(sluggifiedCity);
+         }
+        localStorage.setItem("uacodes", JSON.stringify(uaCodes)); 
+    }
+    uaCodes = JSON.parse(localStorage.getItem("uacodes"));
+    console.log(uaCodes);
     //ADD IN MEANS TO LIMIT AMOUNT OF LISTED CITIES TO 3 AND ONLY 3 . . . CITYLIST.LENGTH < 3
     
-}
 
-addCityButton.addEventListener("click", sluggifyInput);
+
+addCityButton.addEventListener("click", addCityToSearch);
+clearButton.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+})
