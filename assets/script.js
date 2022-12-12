3e796e68494febb3966fe379eeae88744bd338f9
luluco92 <117111188+luluco92@users.noreply.github.com>
2022-12-12 16:26:45 -0500

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,20 @@ var cityList = document.querySelector("#cities-in-chart");
 
 var teleportApiUrl = "https://api.teleport.org/api/urban_areas/" 
 var uaCodes = [];
-//var selectedCities=[];
+var selectedCities=[];
 
 function getUaCodes () {
     uaCodes = JSON.parse(localStorage.getItem("uacodes"))
 }
+
+function getCity () {
+    if (localStorage.getItem("selected cities")){
+    selectedCities = JSON.parse(localStorage.getItem("selected cities"));
+    }
+}
+
+
+
 function sluggifyInput () { //THIS FUNCTION NEEDS TO BE EDITED
     // for(var i = 0; i < rawCityInput.length; i++) {
     //     rawCityInput = rawCityInput.replace(" ", "-")
@@ -19,94 +28,12 @@ function sluggifyInput () { //THIS FUNCTION NEEDS TO BE EDITED
     //  console.log(searchInput);
 }
 
-function makeCityInfo () {
-    var cityInfo = [];
-    if (uaCodes.length === 1) {
-        var cityScore = JSON.parse(localStorage.getItem("cityScoreOne"));
-        var cityDetails = JSON.parse(localStorage.getItem("cityDetailsOne"));
-        var cityImages = JSON.parse(localStorage.getItem("cityImagesOne"));
-        var cityName = JSON.parse(localStorage.getItem("selected cities"));
-        var city = {
-            name: cityName,
-            score: cityScore,
-            details: cityDetails,
-            images: cityImages
-        }
-        console.log(city);
-        cityInfo.push(city);
-    }
-    else if (uaCodes.length === 2) {
-        var cityNames = JSON.parse(localStorage.getItem("selected cities"));
-        var cityOneScore = JSON.parse(localStorage.getItem("cityScoreOne"));
-        var cityOneImages = JSON.parse(localStorage.getItem("cityImagesOne"));
-        var cityOneDetails = JSON.parse(localStorage.getItem("cityDetailsOne"));
-        var cityOneName = cityNames[0];
-        var cityTwoScore = JSON.parse(localStorage.getItem("cityScoreTwo"));
-        var cityTwoImages = JSON.parse(localStorage.getItem("cityImagesTwo"));
-        var cityTwoDetails = JSON.parse(localStorage.getItem("cityDetailsTwo"));
-        var cityTwoName = cityNames[1];
-        var cityOne = {
-            name: cityOneName,
-            score: cityOneScore,
-            details: cityOneDetails,
-            images: cityOneImages
-        };
-        var cityTwo = {
-            name: cityTwoName,
-            score: cityTwoScore,
-            details: cityTwoDetails,
-            images: cityTwoImages
-        }
-        cityInfo.push(cityOne);
-        cityInfo.push(cityTwo);
-    }
-    else if (uaCodes.length === 3) {
-        var cityNames = JSON.parse(localStorage.getItem("selected cities"));
-        var cityOneScore = JSON.parse(localStorage.getItem("cityScoreOne"));
-        var cityOneImages = JSON.parse(localStorage.getItem("cityImagesOne"));
-        var cityOneDetails = JSON.parse(localStorage.getItem("cityDetailsOne"));
-        var cityOneName = cityNames[0];
-        var cityTwoScore = JSON.parse(localStorage.getItem("cityScoreTwo"));
-        var cityTwoImages = JSON.parse(localStorage.getItem("cityImagesTwo"));
-        var cityTwoDetails = JSON.parse(localStorage.getItem("cityDetailsTwo"));
-        var cityTwoName = cityNames[1];
-        var cityThreeScore = JSON.parse(localStorage.getItem("cityScoreThree"));
-        var cityThreeImages = JSON.parse(localStorage.getItem("cityImagesThree"));
-        var cityThreeDetails = JSON.parse(localStorage.getItem("cityDetailsThree"));
-        var cityThreeName = cityNames[2];
-        var cityOne = {
-            name: cityOneName,
-            score: cityOneScore,
-            details: cityOneDetails,
-            images: cityOneImages
-        };
-        var cityTwo = {
-            name: cityTwoName,
-            score: cityTwoScore,
-            details: cityTwoDetails,
-            images: cityTwoImages
-        }
-        var cityThree = {
-            name: cityThreeName,
-            score: cityThreeScore,
-            details: cityThreeDetails,
-            images: cityThreeImages
-        }
-        cityInfo.push(cityOne);
-        cityInfo.push(cityTwo);
-        cityInfo.push(cityThree);
-    }
-console.log(cityInfo);
-localStorage.setItem("city info", JSON.stringify(cityInfo));
-}
-
 function addCityToSearch () {
     var rawCityInput = cityInput.value;
     if (!rawCityInput) {
         return;
     }
-    console.log(rawCityInput);
-    var selectedCities = JSON.parse(localStorage.getItem("selected cities"));
+    getCity();
     if (!selectedCities) {
         selectedCities = [];
     }
@@ -119,6 +46,7 @@ function addCityToSearch () {
     var listedCity = document.createElement("li");
     listedCity.textContent = selectedCities[i];
     cityList.append(listedCity);
+    listedCity.innerHTML += '<span onclick="closeli(this)" style="float:right;cursor:pointer;">X</span>';
     }
     uaCodes = [];
     for(var i = 0; i < selectedCities.length; i++) {
@@ -133,6 +61,22 @@ function addCityToSearch () {
         uaCodes = [];
     }
     
+function closeli(x) {
+    for (i = 0; i < cityList.childElementCount; i++){
+       if (cityList.children[i] === x.parentElement) {
+       getCity();
+       selectedCities.splice(i,1);
+       localStorage.setItem("selected cities", JSON.stringify(selectedCities));
+       getUaCodes();
+       uaCodes.splice(i,1);
+       localStorage.setItem("uacodes", JSON.stringify(uaCodes));
+
+       }
+    }
+    cityInput.value = "";
+    x.parentElement.remove();
+}
+
     //ADD IN MEANS TO LIMIT AMOUNT OF LISTED CITIES TO 3 AND ONLY 3 . . . CITYLIST.LENGTH < 3
     
 
@@ -142,6 +86,8 @@ clearButton.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 })
+
+
 searchButton.addEventListener("click", async function () {
     getUaCodes();
     if (!uaCodes) {
@@ -247,17 +193,7 @@ searchButton.addEventListener("click", async function () {
             localStorage.setItem("cityImagesThree", JSON.stringify(cityImagesThree));
         })
         }
-        //makeChart()
-        makeCityInfo()
+        makeChart()
    }
 })
-
-$(function () {
-    var uaNames = [
-        "Aarhus", "Adelaide", "Albuquerque", "Almaty", "Amsterdam", "Anchorage", "Andorra", "Ankara", "Asheville", "Asuncion", "Athens", "Atlanta", "Auckland", "Austin", "Baku", "Bali", "Baltimore", "Bangkok", "Barcelona", "Beijing", "Beirut", "Belfast", "Belgrade", "Belize City", "Bengaluru", "Bergen", "Berlin", "Bern", "Bilbao", "Birmingham", "Birmingham AL", "Bogota", "Boise", "Bologna", "Bordeaux", "Boston", "Boulder", "Bozeman", "Bratislava", "Brighton", "Brisbane", "Bristol", "Brno", "Brussels", "Bucharest", "Budapest", "Buenos Aires", "Buffalo", "Cairo", "Calgary", "Cambridge", "Cape Town", "Caracas", "Cardiff", "Casablanca", "Charleston", "Charlotte", "Chattanooga", "Chennai", "Chiang Mai", "Chicago", "Chisinau", "Christchurch", "Cincinnati", "Cleveland", "Cluj Napoca", "Cologne", "Colorado Springs", "Columbus", "Copenhagen", "Cork", "Curitiba", "Dallas", "Dar es Salaam", "Delhi", "Denver", "Des Moines", "Detroit", "Doha", "Dresden", "Dubai", "Dublin", "Dusseldork", "Edinburgh", "Edmonton", "Eindhoven", "Eugene", "Florence", "Florianopolis", "Fort Collins", "Frankfurt", "Fukuoka", "Gaillimh", "Gdansk", "Geneva", "Gibraltar", "Glasgow", "Gothenburg", "Grenoble", "Guadalajara", "Guatemala City", "Halifax", "Hamburg", "Hannover", "Havana", "Helsinki", "Ho Chi Minh City", "Hong Kong", "Honolulu", "Houston", "Hyderabad", "Indianapolis", "Innsbruck", "Istanbul", "Jacksonville", "Jakarta", "Johannesburg", "Kansas City", "Karlsruhe", "Kathmandu", "Kiev", "Kingston", "Knoxville", "Krakow", "Kuala Lumpur", "Kyoto", "Lagos", "La Paz", "Las Palmas de Gran Canaria", "Las Vegas", "Lausanne", "Leeds", "Leipzig", "Lille", "Lima", "Lisbon", "Liverpool", "Ljubljana", "London", "Los Angeles", "Louisville", "Luxembourg", "Lviv", "Lyon", "Madison", "Madrid", "Malaga", "Malmo", "Managua", "Manchester", "Manila", "Marseille", "Medellin", "Melbourne", "Memphis", "Mexico City", "Miami", "Milan", "Milwaukee", "Minneapolis Saint Paul", "Minsk", "Montevideo", "Montreal", "Moscow", "Mumbai", "Munich", "Nairobi", "Nantes", "Naples", "Nashville", "New Orleans", "New York", "Nice", "Nicosia", "Oklahoma City", "Omaha", "Orlando", "Osaka", "Oslo", "Ottawa", "Oulu", "Oxford", "Palo Alto", "Panama", "Paris", "Perth", "Philadelphia", "Phnom Penh", "Phoenix", "Phuket", "Pittsburgh", "Portland ME", "Portland OR", "Porto", "Porto Alegre", "Prague", "Providence", "Quebec", "Quito", "Raleigh", "Reykjavik", "Richmond", "Riga", "Rio De Janeiro", "Riyadh", "Rochester", "Rome", "Rotterdam", "Saint Petersburg", "Salt Lake City", "San Antonio", "San Diego", "San Francisco Bay Area", "San Jose", "San Juan", "San Luis Obispo", "San Salvador", "Santiago", "Santo Domingo", "Sao Paulo", "Sarajevo", "Saskatoon", "Seattle", "Seoul", "Seville", "Shanghai", "Singapore", "Skopje", "Sofia", "St Louis", "Stockholm", "Stuttgart", "Sydney", "Taipei", "Tallinn", "Tampa Bay Area", "Tampere", "Tartu", "Tashkent", "Tbilisi", "Tehran", "Tel Aviv", "The Hague", "Thessaloniki", "Tokyo", "Toronto", "Toulouse", "Tunis", "Turin", "Turku", "Uppsala", "Utrecht", "Valencia", "Valletta", "Vancouver", "Victoria", "Vienna", "Vilnius", "Warsaw", "Washington DC", "Wellington", "Winnipeg", "Wroclaw", "Yerevan", "Zagreb", "Zurich" 
-    ];
-    $("#city-input").autocomplete({
-        source: uaNames,
-    });
-});
 
